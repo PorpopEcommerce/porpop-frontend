77835f1259d34e33a0f9b1351199418b1f4ba4b7
Alfred <fredileader@gmail.com>
2024-10-28 04:40:07 +0100

    make code look cleaner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Sidebar from './components/Sidebar'
-import ClientLayout from './clientLayout'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Sidebar from './components/Sidebar';
+import ClientLayout from './ClientLayout';
 
 
 const inter = Inter({ subsets: ['latin'] })
