b9b995354a6023e1dafd0261227ea4b678de33c6
Antipas <benantipas@gmail.com>
2024-12-09 22:58:31 +0100

    added ScrollToTop button

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,22 +1,23 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
-import NavBar from './NavBar'
-import MenuSideComponent from './components/nav/MenuSideComponent'
-import CartSideComponent from './components/nav/CartSideComponent'
-import Login_RegistrationSideComponent from './components/nav/Login_RegistrationSideComponent'
-import Sidebar from './components/Sidebar'
-import CartProvider from './provider/CartProvider'
-import { AuthProvider } from './context/AuthContext'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { useState } from 'react';
+import NavBar from './NavBar';
+import MenuSideComponent from './components/nav/MenuSideComponent';
+import CartSideComponent from './components/nav/CartSideComponent';
+import Login_RegistrationSideComponent from './components/nav/Login_RegistrationSideComponent';
+import Sidebar from './components/Sidebar';
+import CartProvider from './provider/CartProvider';
+import { AuthProvider } from './context/AuthContext';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import ScrollToTop from './components/ScrollToTop'; 
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
     const [menuDisplay, setMenuDisplay] = useState(false);
     const [cartDisplay, setCartDisplay] = useState(false);
     const [signInDisplay, setSignInDisplay] = useState(false);
 
-    // Toggle function to pass to NavBar
+    // Toggle functions to pass to NavBar
     const toggleMenu = () => setMenuDisplay(prev => !prev);
     const toggleCart = () => setCartDisplay(prev => !prev);
     const toggleSignIn = () => setSignInDisplay(prev => !prev);
@@ -25,11 +26,11 @@ const ClientLayout = ({ children }: { children: React.ReactNode }) => {
         <Provider store={store}>
             <AuthProvider>
                 <CartProvider>
-                    <section className='flex'>
+                    <section className="flex">
                         <aside>
                             <Sidebar />
                         </aside>
-                        <main className='flex-1 lg:ml-14 relative'>
+                        <main className="flex-1 lg:ml-14 relative">
                             <div className="relative">
                                 <header>
                                     <NavBar
@@ -42,18 +43,14 @@ const ClientLayout = ({ children }: { children: React.ReactNode }) => {
                                 {cartDisplay && <CartSideComponent toggleCart={toggleCart} />}
                                 {signInDisplay && <Login_RegistrationSideComponent toggleSignIn={toggleSignIn} />}
                                 {children}
+                                <ScrollToTop /> 
                             </div>
                         </main>
                     </section>
                 </CartProvider>
-
-
             </AuthProvider>
-
-
         </Provider>
+    );
+};
 
-    )
-}
-
-export default ClientLayout
+export default ClientLayout;
