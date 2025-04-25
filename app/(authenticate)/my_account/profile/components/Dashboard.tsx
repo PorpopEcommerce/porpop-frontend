afa103c8a79e5f30a3c96e6a67e589add4338bdb
Antipas Ben <benantipas@gmail.com>
2025-04-25 10:34:10 +0100

    l commit

--- app/(authenticate)/my_account/profile/components/Dashboard.tsx
+++ app/(authenticate)/my_account/profile/components/Dashboard.tsx
@@ -43,7 +43,7 @@ const Dashboard = () => {
   
     setTimeout(() => {
       if (user?.role === 'vendor') {
-        router.push("/dashboard/vendor");
+        router.push("/dashboard/VendorDashboard");
       } else if (hasSubscription === true) {
         router.push("/dashboard");
       } else {
